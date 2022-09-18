8277c8cecb460f0b7dda87874c479f95d4b7aab4
nou-ros <nouros44@gmail.com>
2022-09-18 09:44:38 +0600

    second commit

--- src/test/java/com/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/springjenkins/SpringJenkinsApplicationTests.java
@@ -14,6 +14,8 @@ class SpringJenkinsApplicationTests {
     @Test
     void contextLoads() {
         logger.info("Test case executing...");
+        logger.info("Test case executing. Second log statement... ");
+
         assertEquals(true,true);
 
     }
